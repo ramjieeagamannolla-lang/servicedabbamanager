aa7a28ade4e55cc02b66f3a38fd2f35965ff4474
ramjieeagamannolla-lang <ramjieeagamannolla@gmail.com>
2026-06-14 15:40:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,39 @@
+import dotenv from 'dotenv'
+dotenv.config() 
+
 import express from 'express'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
 import cors from 'cors'
 import authRoutes from './routes/authroute.js'
 import mealRoutes from './routes/mealroute.js'
 import cartRoutes from './routes/cartroute.js'
 import orderRoutes from './routes/orderroute.js'
+
 const app = express()
+
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || origin.endsWith(".vercel.app") || origin.includes("localhost")) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS blocked: " + origin));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+    origin: (origin, callback) => {
+        if (!origin || origin.endsWith(".vercel.app") || origin.includes("localhost")) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS blocked: " + origin));
+        }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json())
 
-// routes
 app.use('/api/auth', authRoutes)
 app.use('/api/meals', mealRoutes)
 app.use('/api/cart', cartRoutes)
 app.use('/api/orders', orderRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Service Dabba API is running!')
+})
 
 async function connectDB() {
     try {
@@ -40,7 +46,7 @@ async function connectDB() {
 }
 
 connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`Server running on port ${process.env.PORT}`)
     })
 })
